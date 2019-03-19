10ff42d2e10a4c1c73f44cb005e67a684ea5d5b3
ethanvdb <ethanvdb@hotmail.com>
2019-03-19 11:05:03 +0100

    activity form

--- clubInno/src/Controller/ActivityController.php
+++ clubInno/src/Controller/ActivityController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Activity;
+use App\Form\ActivityType;
 
 class ActivityController extends AbstractController
 {
@@ -15,5 +18,32 @@ class ActivityController extends AbstractController
         return $this->render('activity/index.html.twig', [
             'controller_name' => 'ActivityController',
         ]);
+    }
+
+    /**
+     * @Route("/activity/new", name="activity_new")
+     */
+    public function newActivity(Request $request)
+    {
+        $activity = new Activity();
+
+        $form = $this->createForm(ActivityType::class, $activity);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $activity = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($activity);
+            $em->flush();
+
+            return $this->redirectToRoute('activity');
+        }
+
+        return $this->render('activity/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
     }
 }
