dab7dbf7071d302a8595ecd60525a68e11d039b4
Cel Pynenborg <celpynenborg@gmail.com>
2019-03-20 09:50:16 +0100

    Blogpost Entity and Associations

---
+++ clubInno/src/Entity/BlogPost.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BlogPostRepository")
+ */
+class BlogPost
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    /**
+     * Many blogposts have one activity. This is the owning side.
+     * @ORM\ManyToOne(targetEntity="Activity", inversedBy="blogPosts")
+     * @ORM\JoinColumn(name="activity_id", referencedColumnName="id")
+     */
+    private $activity;
+
+    /**
+     * Many blogposts have one user. This is the owning side.
+     * @ORM\ManyToOne(targetEntity="User", inversedBy="blogPosts")
+     * @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+}
