e68bece536fe47ff0e3b18f18ffe6e3003172900
ethanvdb <ethanvdb@hotmail.com>
2019-03-29 15:24:34 +0100

    guards blog

--- clubInno/src/Controller/BlogPostController.php
+++ clubInno/src/Controller/BlogPostController.php
@@ -8,6 +8,7 @@ use App\Form\BlogPostType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class BlogPostController extends AbstractController
 {
@@ -29,7 +30,8 @@ class BlogPostController extends AbstractController
      */
     public function newBlogPost(Request $request, \Swift_Mailer $mailer)
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $this->denyAccessUnlessGranted('ROLE_TEACHER');
         $user = $this->getUser();
 
         $blogPost = new BlogPost();
@@ -114,72 +116,84 @@ class BlogPostController extends AbstractController
      * @Route("/blog/{id}/delete", requirements={"id": "\d+"}, name="blog_delete")
      */
     public function deleteBlog(BlogPost $blogPost){
-        $em = $this->getDoctrine()->getManager();
-        $comments = $blogPost->getComments();
-        foreach($comments as $comment){
-            $em->remove($comment);
-        }
-        $em->remove($blogPost);
-        $em->flush();
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $user = $this->getUser();
 
-        return $this->redirectToRoute('blog');
+        if(in_array('ROLE_ADMIN', $user->getRoles()) || $user->getId() == $blogPost->getUser()->getId()){
+            $em = $this->getDoctrine()->getManager();
+            $comments = $blogPost->getComments();
+            foreach($comments as $comment){
+                $em->remove($comment);
+            }
+            $em->remove($blogPost);
+            $em->flush();
+
+            return $this->redirectToRoute('blog');
+        }else{
+            throw new AccessDeniedException();
+        }
+
     }
 
     /**
      * @Route("/blog/{id}/edit", requirements={"id": "\d+"}, name="blog_edit")
      */
     public function editBlog(Request $request, BlogPost $blogPost){
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $user = $this->getUser();
 
-        $filenames = $blogPost->getFiles();
+        if(in_array('ROLE_ADMIN', $user->getRoles()) || $user == $blogPost->getUser()) {
+            $filenames = $blogPost->getFiles();
 
-        $files = array();
+            $files = array();
 
-        if ($filenames != null && !empty($filenames)) {
-            foreach ($filenames as $fln) {
-                $fl = $this->getParameter('uploads_directory') . '/' . $fln;
-                array_push($files, $fl);
-            }
-            $blogPost->setFiles($files);
-        } else {
-            $filenames = array();
-        }
-
-        $form = $this->createForm(BlogPostType::class, $blogPost);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $files = $request->files->get('blog_post')['files'];
-            $uploads_directory = $this->getParameter('uploads_directory');
-
-            if ($files != null && !empty($files)) {
-                foreach ($files as $f) {
-                    $fn = md5(uniqid()) . '.' . $f->guessExtension();
-                    array_push($filenames, $fn);
-                    $f->move(
-                        $uploads_directory,
-                        $fn
-                    );
+            if ($filenames != null && !empty($filenames)) {
+                foreach ($filenames as $fln) {
+                    $fl = $this->getParameter('uploads_directory') . '/' . $fln;
+                    array_push($files, $fl);
                 }
+                $blogPost->setFiles($files);
+            } else {
+                $filenames = array();
             }
 
-            $blogPost = $form->getData();
-            $blogPost->setUser($user);
-            $blogPost->setFiles($filenames);
-            $blogPost->setDateTime(new \DateTime('now'));
+            $form = $this->createForm(BlogPostType::class, $blogPost);
 
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($blogPost);
-            $em->flush();
+            $form->handleRequest($request);
 
-            return $this->redirectToRoute('blog');
+            if ($form->isSubmitted() && $form->isValid()) {
+                $files = $request->files->get('blog_post')['files'];
+                $uploads_directory = $this->getParameter('uploads_directory');
+
+                if ($files != null && !empty($files)) {
+                    foreach ($files as $f) {
+                        $fn = md5(uniqid()) . '.' . $f->guessExtension();
+                        array_push($filenames, $fn);
+                        $f->move(
+                            $uploads_directory,
+                            $fn
+                        );
+                    }
+                }
+
+                $blogPost = $form->getData();
+                $blogPost->setUser($user);
+                $blogPost->setFiles($filenames);
+                $blogPost->setDateTime(new \DateTime('now'));
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($blogPost);
+                $em->flush();
+
+                return $this->redirectToRoute('blog');
+            }
+
+            return $this->render('blog_post/edit.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }else{
+            throw new AccessDeniedException();
         }
-
-        return $this->render('blog_post/edit.html.twig', [
-            'form' => $form->createView(),
-        ]);
     }
 
 
