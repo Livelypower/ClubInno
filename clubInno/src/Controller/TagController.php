1ce412c40e0dce0e6ee781ec3973406544bee70a
ethanvdb <ethanvdb@hotmail.com>
2019-03-29 15:37:11 +0100

    guards tag

--- clubInno/src/Controller/TagController.php
+++ clubInno/src/Controller/TagController.php
@@ -6,8 +6,13 @@ use App\Entity\Tag;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
-use App\Form\TagType;
+use App\Form\TagType;use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+/**
+* Require ROLE_ADMIN for *every* controller method in this class.
+*
+* @IsGranted("ROLE_ADMIN")
+*/
 class TagController extends AbstractController
 {
     /**
