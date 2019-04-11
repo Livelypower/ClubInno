3490d6dd013d5db2a8959b6e16c934ff0636364a
ethanvdb <ethanvdb@hotmail.com>
2019-04-11 15:38:28 +0200

    edit/delete activity groups

--- clubInno/src/Controller/ActivityGroupController.php
+++ clubInno/src/Controller/ActivityGroupController.php
@@ -50,6 +50,41 @@ class ActivityGroupController extends AbstractController
     }
 
     /**
+     * @Route("/admin/activities/groups/edit/{id}", name="activity_group_edit")
+     */
+    public function editActivityGroup(Request $request, ActivityGroup $activityGroup)
+    {
+        $form = $this->createForm(ActivityGroupForm::class, $activityGroup);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $activityGroup = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($activityGroup);
+            $em->flush();
+
+            return $this->redirectToRoute('activity_group');
+        }
+        return $this->render('activity_group/edit.html.twig', [
+            'form' => $form->createView(),
+            'group' => $activityGroup
+        ]);
+    }
+
+    /**
+     * @Route("/admin/activities/groups/delete/{id}", name="activity_group_delete")
+     */
+    public function deleteActivityGroup(ActivityGroup $activityGroup){
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($activityGroup);
+        $em->flush();
+
+        return $this->redirectToRoute('activity_group');
+    }
+
+    /**
      * @Route("/admin/activities/groups/assign/{id}", name="activity_group_assign")
      */
     public function assign(Activity $activity){
