51b0b7c177b5c9509f0529c697641b98289ee27f
ethanvdb <ethanvdb@hotmail.com>
2019-04-16 13:40:15 +0200

    form validation semester

--- clubInno/src/Entity/Semester.php
+++ clubInno/src/Entity/Semester.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\SemesterRepository")
@@ -19,11 +21,13 @@ class Semester
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $startYear;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
      */
     private $endYear;
 
